fe3f7ade811e0874ce24744f9bdfdf8283da90f1
Diana Kondratchuk <dianakondratchuk@MacBook-Air-Diana.local>
2023-08-22 17:36:28 +0300

    add page-insurance

---
+++ page-insurance.php
@@ -0,0 +1,252 @@
+<?php /* Template Name: Page Insurance Agency */ ?>
+<?php get_header(); ?>
+<div id="page-insurance-agency">
+<section class="inner-hero" >
+        <div class="hero-back" style="background-image: url(<?php echo get_stylesheet_directory_uri()."/img/accessibility.jpg"; ?>)"></div>
+        <div class="hero-title">
+            <p data-aos="fade-left" style="color: #28404F">  הצהרת   </p>
+            <p data-aos="fade-right"> נגישות </p>
+        </div>
+
+        <div class="inner-hero_mobile">
+            <img src="<?php echo get_stylesheet_directory_uri()."/img/accessibility-mobile.jpg"; ?>" width="100%" alt="">
+        </div>
+    </section>
+
+    <section class="section-form-1">
+        <div class="wrapper-bg"></div>
+        <div class="form-image-1">
+            <img src="" id="form_img_1" alt="">
+        </div>
+        <div class="wrapper">
+            <div class="form-wrapper" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="500">
+                <h2 class="title">
+                    אנחנו כאן בשבילכם!
+                </h2>
+                <?php echo do_shortcode('[contact-form-7 id="7" title="Main form"]')?>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-about section-about_accessibility" >
+        <div class="wrapper">
+            <h1 class="title title-lines" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="500">
+            <span>
+            הסדרי נגישות חברת סלע סוכנות לביטוח בע"מ
+            </span>    
+        </h1>
+            <p class="text-block" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="500">
+            חברת סלע סוכנות לביטוח בע"מ עושה את מירב המאמצים ומשקיעה משאבים רבים על מנת לספק לכל לקוחותיו שירות שוויוני, מכובד, נגיש ומקצועי. בהתאם לחוק שוויון זכויות לאנשים עם מוגבלויות תשנ"ח-1998 ולתקנות שהותקנו מכוחו, מושקעים מאמצים ומשאבים רבים בביצוע התאמות הנגישות הנדרשות שיביאו לכך שאדם בעל מוגבלות יוכל לקבל את השירותים הניתנים לכלל הלקוחות, באופן עצמאי ושוויוני.
+            </p>
+            <p class="text-block" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="500">
+            חברת סלע סוכנות לביטוח בע"מ מעוניינת בשיפור מתמיד. באם נתקלת בקושי או יש לכם הצעת שיפור נשמח לשמוע ממכם, צרו קשר.
+            </p>
+        </div>
+    </section>
+
+    <section class="section-about section-about_accessibility" >
+        <div class="wrapper">
+            <h2 class="title title-lines" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="500">
+                <span>
+            הסדרי נגישות בחנות / בסניף  
+                </span>
+            </h2>
+            <p class="text-block" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="500">
+                <strong>
+                (הערה – במידה ויש לכם מספר מקומות יש להקפיד למלא על כל מקום בנפרד)
+                </strong>
+                להלן הסדרי הנגישות הקיימים במרכז קבלת הקהל הממוקמים בכתובת.............. טלפון....
+            </p>
+            <ul>
+                <li>
+                קיימות XX חניות נכים ב ...... וחניון ..... 
+                </li>
+                <li>
+                קיים רצף גישה ללא מדרגות מהחניה או מהרחוב דרך הכניסה  ועד אלינו (כולל מעליות).
+                </li>
+                <li>
+                קיימות עמדות נגישות לקבלת שירות במרכז קבלת קהל.
+                </li>
+                <li>
+                הותקנו עזרים מסוג "לולאת השראה" ללקויי שמיעה.
+                </li>
+                <li>
+                קיימים שירותי נכים נגישים  [מומלץ לרשום היכן]
+                </li>
+                <li>
+                קיימים שלטי זיהוי והכוונה.
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="section-about section-about_accessibility" >
+        <div class="wrapper">
+            <h2 class="title title-lines" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="500">
+            <span>
+            שירות לקוחות נגיש
+            </span>
+            </h2>
+            <ul>
+                <li>
+                מותרת כניסה למרכז קבלת הקהל לחיית שירות המיועדת לסייע לאדם עם מוגבלות.
+                </li>
+                <li>
+                מותרת כניסת כלבי נחייה לעיוורים
+                </li>
+                <li>
+                ניתן לקבל פטור מתור לזכאים לכך כנגד הצגת תעודה .
+                </li>
+                <li>
+                במקרה בצורך, ניתן לפנות לנציגי השירות בעמדות כדי לקבל סיוע במילוי טפסים.
+                </li>
+                <li>
+                ניתן לפנות לרכז הנגישות לבקש הנגשת מידע לפורמטים רלוונטיים 
+                </li>
+                <li>
+                    <strong>הדרכות עובדים לשירות נגיש</strong>
+                    - מתקיימות הדרכות להכרת תחום הנגישות, יצירת מודעות בקרב העובדים והקניית כלים מעשיים למתן שירות נגיש.
+                </li>
+                <li>
+                    <strong>מוקד טלפוני נגיש</strong>
+                    -  המענה הקולי הונגש כך שהמידע מועבר בשפה ברורה וללא מוסיקת רקע.
+                </li>
+                <li>
+                    <strong>אמצעים נוספים ליצירת קשר</strong>
+                    -  קיימים מספר אמצעים נוספים ליצירת קשר:
+                    <ul>
+                        <li>
+                        שירות פרונטלי - קבלת קהל בימים א'-ה' בין השעות 08:00-18:00
+                        </li>
+                        <li>
+                        טלפון:
+                        </li>
+                        <li>
+                        דואר אלקטרוני:
+                        </li>
+                        <li>
+                        פניה באמצעות "צור קשר" באתר (לינק):
+                        </li>
+                        <li>
+                        צ'אט / ווידיאו ציאט:
+                        </li>
+                        <li>
+                        SMS:
+                        </li>
+                        <li>
+                        פקס:
+                        </li>
+                        <li>
+                        פניה באמצעות הדואר:
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="section-about section-about_accessibility" >
+        <div class="wrapper">
+            <h2 class="title title-lines" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="500">
+            <span>
+            נגישות אתר האינטרנט
+            </span>
+            </h2>
+            <p class="text-block" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="500">
+            אתר אינטרנט נגיש הוא אתר המאפשר לאנשים עם מוגבלות ולאנשים מבוגרים לגלוש באותה רמה של יעילות והנאה ככל הגולשים., בישראל כ – 1.6 מיליון אנשים עם מוגבלות, מתוכם  כ- 751 אלף אנשים בגילאי עבודה 20-64. הנתונים הסטטיסטיים מעידים על פערים גדולים בין אנשים עם מוגבלות לאנשים ללא מוגבלות בכל תחומי החיים.
+            </p>
+            <p class="text-block" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="500">
+            חברת .......... מאמינה ופועלת למען שוויון הזדמנויות במרחב האינטרנטי לבעלי לקויות מגוונות ואנשים הנעזרים בטכנולוגיה מסייעת לשימוש במחשב.
+            </p>
+        </div>
+        <div class="wrapper">
+            <h3 class="title">מידע על נגישות האתר</h3>
+            <ul>
+                <li>
+                אתר זה עומד בדרישות תקנות שיוויון זכויות לאנשים עם מוגבלות (התאמות נגישות לשירות), התשע"ג 2013.
+                </li>
+                <li>
+                התאמות הנגישות בוצעו עפ"י המלצות התקן הישראלי (ת"י 5568) לנגישות תכנים באינטרנט ברמת AA ומסמך WCAG2.0 הבינלאומי.
+                </li>
+                <li>
+                הבדיקות נבחנו לתאימות הגבוהה ביותר עבור דפדפן כרום.
+                </li>
+                <li>
+                האתר מספק מבנה סמנטי עבור טכנולוגיות מסייעות ותמיכה בדפוס השימוש המקובל להפעלה עם מקלדת בעזרת מקשי החיצים, Enter ו- Esc ליציאה מתפריטים וחלונות.
+                </li>
+                <li>
+                מותאם לתצוגה בדפדפנים הנפוצים ולשימוש בטלפון הסלולרי.
+                </li>
+                <li>
+                לשם קבלת חווית גלישה מיטבית עם תוכנת הקראת מסך, אנו ממליצים לשימוש בתוכנת NVDA העדכנית ביותר.
+                </li>
+                <li>
+                מסמכים או סרטוני וידאו שעלו לאתר לפני אוקטובר 2017 ייתכן שלא נגישים באופן מלא. במידה שנתקלתם במסמך כזה או בסרטון, תוכלו לפנות לרכזת נגישות ב ............. ואנחנו נדאג להנגיש לכם את המידע.
+                </li>
+                <li>
+                מסירת מידע בפורמט נגיש - החברה מעמידה עבור לקוחותיה אפשרות לקבלת מידע בפורמטים נגישים. מסירת המידע הינה ללא עלות ומיועדת עבור אנשים עם מוגבלות. לפניות ומידע בנושא נגישות ניתן ליצור קשר עם רכז הנגישות של החברה שפרטיו מופיעים בהמשך ההצהרה.
+                </li>
+                <li>
+                בדיקות ויעוץ הנגישות בוצעו על ידי חברת A2Z נגישות ושיווק באינטרנט
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="section-about section-about_accessibility" >
+        <div class="wrapper">
+            <h2 class="title title-lines" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="500">
+            <span>
+            דרכי פנייה לבקשות והצעות לשיפור בנושא נגישות
+            </span>
+            </h2>
+            <p class="text-block" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="500">
+            יש לציין כי אנו ממשיכים במאמצים לשפר את נגישות החברה כחלק ממחויבותנו לאפשר לכלל האוכלוסייה כולל אנשים עם מוגבלויות לקבל את השרות הנגיש ביותר.
+            </p>
+            <p class="text-block" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="500">
+            במידה ונתקלת בבעיה או בתקלה כלשהי בנושא הנגישות, נשמח שתעדכן אותנו בכך ואנו נעשה כל מאמץ למצוא עבורך פתרון מתאים ולטפל בתקלה בהקדם ככל שניתן.
+            </p>
+        </div>
+        <div class="wrapper">
+            <h3 class="title">פרטי רכז הנגישות ב......</h3>
+            <ul>
+                <li>
+                שם: 
+                </li>
+                <li>
+                טלפון: 
+                </li>
+                <li>
+                דוא”ל: 
+                </li>
+                <li>
+                ווטסאפ:
+                </li>
+            </ul>
+        </div>
+        <div class="wrapper">
+            <h3 class="title">פרסום הצהרת הנגישות</h3>
+            <p class="text-block" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="500">
+            הצהרת הנגישות עודכנה ביום: XXXX
+            </p>
+        </div>
+    </section>
+
+    <section class="section-form-2">
+        <div class="wrapper-bg"></div>
+        <div class="form-image-2">
+            <img src="" id="form_img_1" alt="">
+        </div>
+        <div class="wrapper">
+            <div class="form-wrapper" data-aos="fade-left">
+                <h2 class="title">
+                    אנחנו כאן בשבילכם!
+                </h2>
+                <?php echo do_shortcode('[contact-form-7 id="7" title="Main form"]') ?>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+<?php get_footer(); ?>
